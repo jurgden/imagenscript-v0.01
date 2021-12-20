ddce2373b3f54be12189ed690116d69e266a1aec
Jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-20 18:21:40 +0000

    automated an entire login process using selenium

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
+
+
+
+def get_driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument('disable-infobars')
+  options.add_argument('start-maximized')
+  options.add_argument('disable-dev-shm-usage')
+  options.add_argument('no-sandbox')
+  options.add_experimental_option('excludeSwitches', ['enable-automation'])
+  options.add_argument('disable-blink-features=AutomationControlled')
+  driver = webdriver.Chrome(options=options)
+  driver.get('https://automated.pythonanywhere.com/login/')
+  return driver 
+
+def main():
+  driver = get_driver()
+  driver.find_element(by='id', value='id_username').send_keys('automated')
+  time.sleep(2)
+  driver.find_element(by='id', value='id_password').send_keys('automatedautomated' + Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(by='xpath', value='/html/body/nav/div/a').click()
+  print(driver.current_url)
+
+print(main())
